272e5caac8e24d98ff9b53fe3e00f3ca09f5d1d1
Kareem <kareem@Kareems-MacBook-Pro.local>
2022-11-02 12:49:17 +0300

    Completed react hooks lesson

--- Intro-to-React-Hooks/student/react-hooks/src/App.js
+++ Intro-to-React-Hooks/student/react-hooks/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import BatteryContainer from './components/BatteryContainer/BatteryContainer';
+import BatteryHookContainer from './components/BatteryHookContainer/BatteryHookContainer';
+import BatteryOne from './components/BatteryOne/BatteryOne';
+
+export const UserContext = createContext()
 
 function App() {
+  const [user, setUser] = useState({
+    name: 'Alex',
+    email: 'Alex@GA.com'
+  })
+  
+  
   return (
     <>
+    <UserContext.Provider value={user}>
+      <BatteryOne />
+    </UserContext.Provider>
       <BatteryContainer />
+      <BatteryHookContainer />
     </>
   );
 }
