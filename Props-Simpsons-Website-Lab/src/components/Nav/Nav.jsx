fc5847d1ec615622ffdb40ab3a07bf39c5b85335
mahdiatubly <mahdiatubly@gmail.com>
2022-11-02 09:08:14 +0300

    Completing Props-Simpsons-Website-Lab

--- Props-Simpsons-Website-Lab/src/components/Nav/Nav.jsx
+++ Props-Simpsons-Website-Lab/src/components/Nav/Nav.jsx
@@ -1,10 +1,13 @@
 import './Nav.css'
+import Menue from './Menue'
 
 export default function Nav(props) {
     return(
         <nav className="nav-bar">
             <div className="upper-nav">
-                <div className="logo-nav"></div>
+                <div className="logo-nav">
+                    <h1>BH GA CLASS</h1>
+                </div>
                 <div className="right-upper-nav-wrapper">
                     <div className="link-socialmedia">
                         <i className="fa fa-facebook"></i>
@@ -19,12 +22,7 @@ export default function Nav(props) {
                     <div className="right-upper-nav">
                         <ul className="cover-contact-about">
                             <li className="trigger"><a href="/#">VISIT SPRINGFIELD!</a></li>
-                            <li className="trigger"><a href="/#">MENU ITEM</a>
-                                <ul className="submenu">
-                                    <li><a href="/#">Link 1</a></li>
-                                    <li><a href="/#">Link 2</a></li>
-                                </ul>
-                            </li>
+                            <Menue />
                         </ul>
                         <button className="btn-donate">DONATE</button>
                     </div>
@@ -33,42 +31,12 @@ export default function Nav(props) {
 
             <div className="lower-nav">
                 <ul className="lower-nav-ul">
-                    <li className="trigger"><a href="/#">MENU ITEM</a>
-                        <ul className="submenu">
-                            <li><a href="/#">Link 1</a></li>
-                            <li><a href="/#">Link 2</a></li>
-                        </ul>
-                    </li>
-                    <li className="trigger"><a href="/#">MENU ITEM</a>
-                        <ul className="submenu">
-                            <li><a href="/#">Link 1</a></li>
-                            <li><a href="/#">Link 2</a></li>
-                        </ul>
-                    </li>
-                    <li className="trigger"><a href="/#">MENU ITEM</a>
-                        <ul className="submenu">
-                            <li><a href="/#">Link 1</a></li>
-                            <li><a href="/#">Link 2</a></li>
-                        </ul>
-                    </li>
-                    <li className="trigger"><a href="/#">MENU ITEM</a>
-                        <ul className="submenu">
-                            <li><a href="/#">Link 1</a></li>
-                            <li><a href="/#">Link 2</a></li>
-                        </ul>
-                    </li>
-                    <li className="trigger"><a href="/#">MENU ITEM</a>
-                        <ul className="submenu">
-                            <li><a href="/#">Link 1</a></li>
-                            <li><a href="/#">Link 2</a></li>
-                        </ul>
-                    </li>
-                    <li className="trigger"><a href="/#">MENU ITEM</a>
-                        <ul className="submenu">
-                            <li><a href="/#">Link 1</a></li>
-                            <li><a href="/#">Link 2</a></li>
-                        </ul>
-                    </li>
+                    <Menue />
+                    <Menue />
+                    <Menue />
+                    <Menue />
+                    <Menue />
+                    <Menue />
                     <i className="fa fa-search"></i>            
                 </ul>
             </div>
---
+++ React-Floorplan-Lab/react-floorplan-lab/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+
+function App() {
+  class App extends Component{
+    render() {
+      return (
+        <h1>Love!</h1>
+      )
+        
+      
+    }
+  } 
+  
+}
+
+export default App;
