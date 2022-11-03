8e7be8bc0ec20a65e62143f497f99518fd8bb2bc
Loooai10 <honey_moonn@yahoo.com>
2022-11-03 09:16:11 +0300

    updated

--- Props-Simpsons-Website-Lab/src/App.js
+++ Props-Simpsons-Website-Lab/src/App.js
@@ -20,6 +20,11 @@ function App() {
               <ProfileCard />
               <ProfileCard />
               <ProfileCard pic={bart} />
+              <ProfileCard pic={homer} name={'Homer'} email={'homer@simposon.coom'} description={'Loves his familv as much as donuts'} />
+              <ProfileCard pic={marge} name={'Marge'} email={'marge@simposon.coom'} description={'Marge is a role model.'} />
+              <ProfileCard pic={bart} name={'Bart'} email={'bart@simposon.coom'} description={'Bart is the eternal prankster'} />
+              <ProfileCard pic={lisa} name={'Lisa'} email={'lisa@simposon.coom'}  description={'Amazing saxophone player.'} />
+              <ProfileCard pic={maggie} name={'Maggie'} email={'maggie@simposon.coom'} description={'Maggie is known to have murdered at least one person'} />
             </div>
           </div>
       </section>
@@ -28,3 +33,5 @@ function App() {
 }
 
 export default App;
+
+             
